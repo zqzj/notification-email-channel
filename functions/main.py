bc942efe1bc854192e613d339215d822c8f0d1a3
Aharon Louzon <aharon@dataloop.ai>
2023-05-21 12:22:50 +0300

    done

--- functions/main.py
+++ functions/main.py
@@ -7,6 +7,6 @@ class ServiceRunner(dl.BaseServiceRunner):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def notify(self, message, message_context, target, subscription, **kwargs):
+    def notify(self, message: str, recipients: List[str], subscription=None, message_context=None, **kwargs):
         # implement your logic here
         pass
