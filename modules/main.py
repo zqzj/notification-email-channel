56a6840cdc16887b1f9dd4984ddc659bb831c542
shlomi <shlomi.s@dataloop.ai>
2023-05-28 16:07:31 +0300

    input modifications

--- modules/main.py
+++ modules/main.py
@@ -1,35 +1,31 @@
 
 import dtlpy as dl
 
-class NotificationInfo:
-    def __init__(self, notification_info: dict):
-        self.notification_info = notification_info
-        self.notification_info = notification_info.get('notificationId', None)
-        self.notification_info = notification_info.get('notificationCode', None)
-        self.notification_info = notification_info.get('type', None)
-        self.notification_info = notification_info.get('context', None)
-        self.notification_info = notification_info.get('priority', None)
-        self.notification_info = notification_info.get('eventMessage', None)
-
 class EventMessage:
     def __init__(self, event_message: dict):
-        self.event_message = event_message
-        self.event_message = event_message.get('title', None)
-        self.event_message = event_message.get('description', None)
-        self.event_message = event_message.get('resourceAction', None)
-        self.event_message = event_message.get('resourceId', None)
-        self.event_message = event_message.get('resourceType', None)
-        self.event_message = event_message.get('resourceName', None)
+        self.title = event_message.get('title', None)
+        self.description = event_message.get('description', None)
+        self.resourceAction = event_message.get('resourceAction', None)
+        self.resourceId = event_message.get('resourceId', None)
+        self.resourceType = event_message.get('resourceType', None)
+        self.resourceName = event_message.get('resourceName', None)
+
+class NotificationInfo:
+    def __init__(self, notification_info: dict):
+        self.notificationCode = notification_info.get('notificationCode', None)
+        self.type = notification_info.get('type', None)
+        self.context = notification_info.get('context', None)
+        self.priority = notification_info.get('priority', None)
+        self.eventMessage = EventMessage(notification_info.get('eventMessage', None))
+
 
 class ApplicationInput:
     def __init__(self, application_input: dict):
-        self.application_input = application_input
-        self.application_input = application_input.get('notificationInfo', None)
-        self.application_input = application_input.get('recipients', None)
-        self.application_input = application_input.get('notificationId', None)
+        self.notification_info = NotificationInfo(application_input.get('notificationInfo', None))
+        self.recipients = application_input.get('recipients', None)
+        self.notificationId = application_input.get('notificationId', None)
 
 class ServiceRunner(dl.BaseServiceRunner):
-    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
